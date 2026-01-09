a3de97f60297412e8fe42de15e0e06db995b5a0c
rehemuWulugebige <rehemu.wulugebige@gmail.com>
2026-01-08 21:46:12 -0500

    2.1.3 selecting html with ID using css

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -9,6 +9,17 @@ export default function Lab2() {
         Although it&apos;s very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
+      <div id="wd-css-id-selectors">
+        <h3>ID selectors</h3>
+        <p id="wd-id-selector-1">
+          Instead of changing the look and feel of all the elements of the same
+          name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="wd-id-selector-2">
+          Here&apos;s another paragraph using a different ID and a different
+          look and feel
+        </p>
+      </div>
     </div>
   );
 }
